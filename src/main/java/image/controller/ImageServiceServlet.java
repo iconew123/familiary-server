14b029618c987bd666186b0d3df5b8b89681643d
ksy7615 <sykim7615@daum.net>
2024-05-22 18:19:20 +0900

    Add: Image controllers

---
+++ src/main/java/image/controller/ImageServiceServlet.java
@@ -0,0 +1,40 @@
+package image.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import util.Action;
+
+public class ImageServiceServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String command = request.getPathInfo();
+		String method = request.getMethod();
+		
+		System.out.println("command : "+command);
+		System.out.println("method : "+method);
+		
+		ImageActionFactory af = ImageActionFactory.getInstance();
+		Action action = af.getAction(command, method);
+		
+		if(action != null) {
+			action.execute(request, response);
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		doGet(request, response);
+	}
+	
+	@Override
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
