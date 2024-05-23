7cc1a7574e0fb04b9d1ae97152ef90c33c24fcb1
ksy7615 <sykim7615@daum.net>
2024-05-23 10:38:00 +0900

    Modify: community R in CommunityDao

--- src/main/java/community/model/CommunityDao.java
+++ src/main/java/community/model/CommunityDao.java
@@ -1,12 +1,9 @@
 package community.model;
 
-import java.sql.Blob;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +29,7 @@ public class CommunityDao {
 	public List<CommunityResponseDto> findCommunityAll() {
 		List<CommunityResponseDto> list = new ArrayList<CommunityResponseDto>();
 		conn = DBManager.getConnection();
-		String sql = "SELECT code, user_id, user_nickname, title, content, photo, category, reg_date FROM community WHERE category='잡담' order by reg_date desc";
+		String sql = "SELECT code, user_id, user_nickname, title, content, category, reg_date FROM community WHERE category='잡담' order by reg_date desc";
 		try {
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery();
@@ -42,11 +39,9 @@ public class CommunityDao {
 				String userNickName = rs.getString(3);
 				String title = rs.getString(4);
 				String content = rs.getString(5);
-				Blob photo = rs.getBlob(6);
-				String tag = rs.getString(7);
-				String comment = rs.getString(8);
-				Timestamp regDate = rs.getTimestamp(9);
-				CommunityResponseDto community = new CommunityResponseDto(code, userId, userNickName, title, content, photo, tag, comment, regDate);
+				String category = rs.getString(6);
+				Timestamp regDate = rs.getTimestamp(7);
+				CommunityResponseDto community = new CommunityResponseDto(code, userId, userNickName, title, content, category, regDate);
 				list.add(community);
 			}
 		} catch (SQLException e) {
@@ -55,89 +50,6 @@ public class CommunityDao {
 			DBManager.close(conn, pstmt, rs);
 		}
 		return list;
-	}
-	
-	// 잡담용 크리에이트 보드
-	public CommunityResponseDto createBoard(CommunityRequestDto CommunityRequestDto) {
-		conn = DBManager.getConnection();
-
-		CommunityResponseDto community = null;
-		String userNickname = CommunityRequestDto.getUserNickname();
-		String title = CommunityRequestDto.getTitle();
-		String content = CommunityRequestDto.getContent();
-		
-		Blob photo = CommunityRequestDto.getPhoto();
-		if(photo == null) {
-			String sql = "INSERT INTO community (user_nickname, title, content, tag) VALUES (?, ?, ?,'잡담')";
-
-			try {
-				pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-				pstmt.setString(1, userNickname);
-				pstmt.setString(2, title);
-				pstmt.setString(3, content);
-
-				pstmt.executeUpdate();
-
-				ResultSet rs = pstmt.getGeneratedKeys();
-				if (rs.next()) {
-					int code = rs.getInt(1);
-					community = findCommunity(code);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} finally {
-				DBManager.close(conn, pstmt, rs);
-			}
-			return community;
-		} else {
-			String sql = "INSERT INTO community (user_nickname, title, content, tag, photo) VALUES (?, ?, ?, '잡담', ?)";
-
-			try {
-				pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-				pstmt.setString(1, userNickname);
-				pstmt.setString(2, title);
-				pstmt.setString(3, content);
-				pstmt.setBlob(4, photo);
-
-				pstmt.executeUpdate();
-
-				ResultSet rs = pstmt.getGeneratedKeys();
-				if (rs.next()) {
-					int code = rs.getInt(1);
-					community = findCommunity(code);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			} finally {
-				DBManager.close(conn, pstmt, rs);
-			}
-			return community;
-		}
-	}
-	
-	//
-	public CommunityResponseDto findCommunity(int code) {
-		conn = DBManager.getConnection();
-		CommunityResponseDto community = null;
-		String sql = "SELECT code, user_nickname, title, content, photo FROM community WHERE code = ?";
-		
-		try {
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, code);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				int communityCode = rs.getInt(1);
-				String userNickname = rs.getString(2);
-				String title = rs.getString(3);
-				String content = rs.getString(4);
-				Blob photo = rs.getBlob(5);
-
-				community = new CommunityResponseDto(communityCode, userNickname, title, content, photo);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return community;
 	}
 	
 }
