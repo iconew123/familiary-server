f2aff724062512ddc5f0b0683975198da05887f0
yoon-wish <sowon5685@gmail.com>
2024-06-05 00:14:42 +0900

    Modify Babyinfo create and read

--- src/main/java/babyInfo/controller/action/BabyInfoReadAction.java
+++ src/main/java/babyInfo/controller/action/BabyInfoReadAction.java
@@ -18,33 +18,39 @@ public class BabyInfoReadAction implements Action {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
-        // CORS 설정
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
 
-        System.out.println("진입1");
-        String babyCode = request.getParameter("code");
-        String date = request.getParameter("date");
-        System.out.println("date: " + date);
+        try {
+            String babyCode = request.getParameter("code");
+            String date = request.getParameter("date");
 
-        System.out.println("진입4");
-        System.out.println("date" + date);
+            if (babyCode == null || date == null) {
+                throw new IllegalArgumentException("code 또는 date 파라미터가 누락되었습니다.");
+            }
 
-        BabyInfoDao dao = new BabyInfoDao();
-        BabyInfo info = dao.findBabyInfoByCodeAndDate(babyCode, date);
+            System.out.println("진입4");
 
-        JSONObject jsonObj = new JSONObject();
-        System.out.println("진입5");
-        jsonObj.put("baby_code", info.getBaby_code());
-        jsonObj.put("date", info.getDate());
-        jsonObj.put("height", info.getHeight());
-        jsonObj.put("weight", info.getWeight());
-        jsonObj.put("spec_note", info.getSpec_note());
+            BabyInfoDao dao = new BabyInfoDao();
+            BabyInfo info = dao.findBabyInfoByCodeAndDate(babyCode, date);
 
-        // 클라이언트로 JSON 응답을 보냄
-        response.getWriter().write(jsonObj.toString());
+            if (info == null) {
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                response.getWriter().write("{\"error\":\"해당하는 정보를 찾을 수 없습니다.\"}");
+                return;
+            }
 
+            JSONObject jsonObj = new JSONObject();
+            jsonObj.put("baby_code", info.getBaby_code());
+            jsonObj.put("date", info.getDate());
+            jsonObj.put("height", info.getHeight());
+            jsonObj.put("weight", info.getWeight());
+            jsonObj.put("spec_note", info.getSpec_note());
+
+            // 클라이언트로 JSON 응답을 보냄
+            response.getWriter().append(jsonObj.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write("{\"error\":\"서버 오류가 발생했습니다.\"}");
+        }
     }
 }
