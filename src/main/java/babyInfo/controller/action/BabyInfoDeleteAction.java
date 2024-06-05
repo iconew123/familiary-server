2817acd55b1b75933143b439ca2cc3e457206518
yoon-wish <sowon5685@gmail.com>
2024-06-05 12:43:49 +0900

    Modify

--- src/main/java/babyInfo/controller/action/BabyInfoDeleteAction.java
+++ src/main/java/babyInfo/controller/action/BabyInfoDeleteAction.java
@@ -16,11 +16,6 @@ public class BabyInfoDeleteAction implements Action {
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.setHeader("Access-Control-Allow-Origin", "*"); // 모든 도메인 허용
-        response.setHeader("Access-Control-Allosw-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-
         String baby_code = request.getParameter("baby_code");
         String date = request.getParameter("date");
 
